8f61705e542e4193f3d6b28a7cdc9aa85b403b71
DanhLe1412 <ltdanh19@apcs.vn>
2022-02-22 17:31:30 +0700

    fix: remove un-used IncreasePC

--- code/userprog/exception.cc
+++ code/userprog/exception.cc
@@ -1,4 +1,4 @@
-// exception.cc 
+// exception.cc
 //	Entry point into the Nachos kernel from user programs.
 //	There are two kinds of things that can cause control to
 //	transfer back to here from user code:
@@ -9,7 +9,7 @@
 //
 //	exceptions -- The user code does something that the CPU can't handle.
 //	For instance, accessing memory that doesn't exist, arithmetic errors,
-//	etc.  
+//	etc.
 //
 //	Interrupts (which can also cause control to transfer from user
 //	code into the Nachos kernel) are handled elsewhere.
@@ -18,7 +18,7 @@
 // Everything else core dumps.
 //
 // Copyright (c) 1992-1996 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
+// All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
 #include "copyright.h"
@@ -33,9 +33,9 @@ void InscreasePC()
 
 	/* set programm counter to next instruction (all Instructions are 4 byte wide)*/
 	kernel->machine->WriteRegister(PCReg, kernel->machine->ReadRegister(PCReg) + 4);
-	  
+
 	/* set next programm counter for brach execution */
-	kernel->machine->WriteRegister(NextPCReg, kernel->machine->ReadRegister(PCReg)+4);
+	kernel->machine->WriteRegister(NextPCReg, kernel->machine->ReadRegister(PCReg) + 4);
 }
 
 //----------------------------------------------------------------------
@@ -52,107 +52,95 @@ void InscreasePC()
 //		arg3 -- r6
 //		arg4 -- r7
 //
-//	The result of the system call, if any, must be put back into r2. 
+//	The result of the system call, if any, must be put back into r2.
 //
 // If you are handling a system call, don't forget to increment the pc
 // before returning. (Or else you'll loop making the same system call forever!)
 //
-//	"which" is the kind of exception.  The list of possible exceptions 
+//	"which" is the kind of exception.  The list of possible exceptions
 //	is in machine.h.
 //----------------------------------------------------------------------
 
 void ExceptionHandler(ExceptionType which)
 {
-    int type = kernel->machine->ReadRegister(2);
+	int type = kernel->machine->ReadRegister(2);
 
-    DEBUG(dbgSys, "Received Exception " << which << " type: " << type << "\n");
+	DEBUG(dbgSys, "Received Exception " << which << " type: " << type << "\n");
 
-    switch (which) {
-    case SyscallException:
-      switch(type) {
-      case SC_Halt:
-		DEBUG(dbgSys, "Shutdown, initiated by user program.\n");
-
-		SysHalt();
-
-		ASSERTNOTREACHED();
-		break;
-
-      case SC_Add:
-		DEBUG(dbgSys, "Add " << kernel->machine->ReadRegister(4) << " + " << kernel->machine->ReadRegister(5) << "\n");
-	
-		/* Process SysAdd Systemcall*/
-		int result;
-		result = SysAdd(/* int op1 */(int)kernel->machine->ReadRegister(4),
-				/* int op2 */(int)kernel->machine->ReadRegister(5));
-
-		DEBUG(dbgSys, "Add returning with " << result << "\n");
-		/* Prepare Result */
-		kernel->machine->WriteRegister(2, (int)result);
-	
-		/* Modify return point */
+	switch (which)
+	{
+	case SyscallException:
+		switch (type)
 		{
-	  	/* set previous programm counter (debugging only)*/
-	  	kernel->machine->WriteRegister(PrevPCReg, kernel->machine->ReadRegister(PCReg));
+		case SC_Halt:
+			DEBUG(dbgSys, "Shutdown, initiated by user program.\n");
 
-	  	/* set programm counter to next instruction (all Instructions are 4 byte wide)*/
-	  	kernel->machine->WriteRegister(PCReg, kernel->machine->ReadRegister(PCReg) + 4);
-	  
-	  	/* set next programm counter for brach execution */
-	  	kernel->machine->WriteRegister(NextPCReg, kernel->machine->ReadRegister(PCReg)+4);
+			SysHalt();
+
+			ASSERTNOTREACHED();
+			break;
+
+		case SC_Add:
+			DEBUG(dbgSys, "Add " << kernel->machine->ReadRegister(4) << " + " << kernel->machine->ReadRegister(5) << "\n");
+
+			/* Process SysAdd Systemcall*/
+			int result;
+			result = SysAdd(/* int op1 */ (int)kernel->machine->ReadRegister(4),
+							/* int op2 */ (int)kernel->machine->ReadRegister(5));
+
+			DEBUG(dbgSys, "Add returning with " << result << "\n");
+			/* Prepare Result */
+			kernel->machine->WriteRegister(2, (int)result);
+
+			InscreasePC();
+			return;
+
+			ASSERTNOTREACHED();
+
+			break;
+
+		default:
+			cerr << "Unexpected system call " << type << "\n";
+			break;
 		}
-
-		return;
-	
-		ASSERTNOTREACHED();
-
-		break;
-
-      default:
-		cerr << "Unexpected system call " << type << "\n";
-		break;
-      }
-	  
-	  InscreasePC();
-      break;
-	
+		
 	case PageFaultException:
 		printf("\nNo valid translation found.\n");
-		ASSERT(FALSE);
+		SysHalt();
 		break;
 
 	case ReadOnlyException:
 		printf("\nWrite attempted to page marked \"read-only\".\n");
-		ASSERT(FALSE);
+		SysHalt();
 		break;
 
 	case BusErrorException:
 		printf("\nTranslation resulted in an invalid physical address.\n");
-		ASSERT(FALSE);
+		SysHalt();
 		break;
 
 	case AddressErrorException:
 		printf("\nUnaligned reference or one that was beyond the end of the address space.\n");
-		ASSERT(FALSE);
+		SysHalt();
 		break;
 
 	case OverflowException:
 		printf("\nInteger overflow in add or sub.\n");
-		ASSERT(FALSE);
+		SysHalt();
 		break;
 
 	case IllegalInstrException:
 		printf("\nUnimplemented or reserved instr\n");
-		ASSERT(FALSE);
+		SysHalt();
 		break;
 
 	case NumExceptionTypes:
 		printf("\nNumExceptionTypes\n");
-		ASSERT(FALSE);
+		SysHalt();
 		break;
-    default:
-      cerr << "Unexpected user mode exception" << (int)which << "\n";
-      break;
-    }
-    ASSERTNOTREACHED();
+	default:
+		cerr << "Unexpected user mode exception" << (int)which << "\n";
+		break;
+	}
+	ASSERTNOTREACHED();
 }
